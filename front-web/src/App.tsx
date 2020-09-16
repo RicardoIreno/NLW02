323c292126b1d1f30f5122f34f17b428ee5847c0
Ricardo Ireno <ricardoireno@outlook.com>
2020-09-16 11:47:22 -0300

    Feat (front-web) Landing page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Routes from './routes';
+import './assets/styles/global.scss'
 
 function App() {
   return (
--- front-web/src/pages/Landing/index.tsx
+++ front-web/src/pages/Landing/index.tsx
@@ -1,8 +1,49 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+// import api from '../../services/api';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './style.scss'
 
 function Landing() {
-  return <h1>Landing</h1>
+
+  return (
+    <div id="page-landing">
+      <div className="container-logo">
+        <img src={logoImg} alt="Proffy" />
+        <h2>Plataforma de fucking estudos</h2>
+      </div>
+
+      <img
+        src={landingImg}
+        alt="Plataforma de estudos"
+        className="hero-image"
+      />
+
+      <div className="container-button">
+        <Link to="./study" className="button-study">
+          <img src={studyIcon} alt="Estudar" />
+             Estudar
+         </Link>
+
+        <Link to="./give-classes" className="button-give-classes">
+          <img src={giveClassesIcon} alt="Dar aulas" />
+             Dar aulas
+         </Link>
+      </div>
+
+      <span className="total-connections">
+       Total de x conexões já realizadas
+         <img src={purpleHeartIcon} alt="Coração" />
+      </span>
+    </div>
+
+  )
 }
 
 export default Landing
