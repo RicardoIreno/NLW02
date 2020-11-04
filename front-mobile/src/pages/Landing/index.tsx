4e733e754175ab0fcfd94071a4d6b14d6aa9209c
Ricardo Ireno <ricardoireno@outlook.com>
2020-11-04 16:12:22 -0300

    Feat (front-mobile) Created 'routes' and 'GiveClasses' for test

--- front-mobile/src/pages/Landing/index.tsx
+++ front-mobile/src/pages/Landing/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react'  
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { RectButton } from 'react-native-gesture-handler'
+import { Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+
 import styles from './styles'
 
 function Landing() {
+  const { navigate } = useNavigation()
+
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses') 
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
        Proffy {'\n'} 
-        <Text style={styles.titleBold}>Nem sei o que escrever XD</Text>
+        <Text style={styles.titleBold}>Eu sou o Landing {'\n'} </Text>
+
+        <TouchableOpacity onPress={handleNavigateToGiveClassesPage}
+          style={[styles.buttonContainer, styles.buttonPrimary]} >
+        
+          <Text>Eu sou um botão</Text>
+        
+        </TouchableOpacity>
       </Text>
     </View>
   )
