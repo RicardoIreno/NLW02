e7d5099ab3d2db0e71b9c4fab7065d8e81083d58
Ricardo Ireno <ricardoireno@outlook.com>
2020-11-19 12:15:34 -0300

    Feat (front-web): search and show teachers in Teachers page

--- front-web/src/components/TeacherItem/index.tsx
+++ front-web/src/components/TeacherItem/index.tsx
@@ -2,21 +2,38 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './style.scss'
 
-function TeacherItem() {
+
+export interface Teacher {
+  id: number
+  avatar: string 
+  bio: String
+  cost: number
+  name: string
+  subject: string
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return(
     <article className="teacher-item">
       <header>
         <img src="" alt="" />
         <div>
-            <strong>Testando</strong>
-            <span>samerda</span>
+            <strong> {teacher.name} </strong>
+            <span> {teacher.subject} </span>
         </div>
       </header>
-      <p></p>
+
+      <p> {teacher.bio}</p>
 
       <footer>
           <p>Preço por hora
-            <strong> 300 conto </strong>
+            <strong> {teacher.cost} </strong>
           </p>
           <button type="button"> 
               <img src={whatsappIcon} alt="Whatsapp"/>
--- front-web/src/pages/TeacherList/index.tsx
+++ front-web/src/pages/TeacherList/index.tsx
@@ -1,19 +1,59 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
 import Input from '../../components/form/input'
 import Select from '../../components/form/select'
 import './style.scss' 
+import api from '../../services/api'
+
+
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([])
+  const [subject, setSubject] = useState('')
+  const [week_day, setWeekDay] = useState('')
+  const [time, setTime] = useState('')
+  
+  async function searchTeachers(e: FormEvent) {
+    e.preventDefault()
+
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time,
+      }
+    })
+    // console.log({
+    //   subject,
+    //   week_day,
+    //   time
+    // })
+    console.log(response.data)
+    setTeachers(response.data)
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">      
         
-        <form id="search-teachers">    
+        <form id="search-teachers"> 
+          <Select 
+              name="subject" 
+              label="Matéria" 
+              value={subject} 
+              onChange={(e) => {setSubject(e.target.value)}} 
+              options={[
+                {value: "Artes", label: "Artes" },
+                {value: "Paleontologia", label: "Paleontologia" },
+                {value: "Criminalistica", label: "Criminalistica" },
+              ]}
+            />
+
           <Select 
             name="week_day"
             label="Dia da semana"
+            onChange={(e) => {setWeekDay(e.target.value) }}
             options={[
               {value: '0', label: 'Domingo'},
               {value: '1', label: 'Segunda'},
@@ -22,18 +62,25 @@ function TeacherList() {
               {value: '4', label: 'Quinta'},
               {value: '5', label: 'Sexta'},
               {value: '6', label: 'Sábado'},
-            ]}/>
+            ]}
+          />
 
-            <Input name="Week_day" label="Dia da semana" />
-            <Input type="time" name="time" label="Hora" />
+          <Input 
+            type="time" 
+            name="time" 
+            label="Hora" 
+            onChange={(e) => {setTime(e.target.value) }}
+          />
+            <button type="submit" onClick={searchTeachers}>Buscar</button>
+            
         </form>
 
-        <main>
-          EI!!!
-          <TeacherItem /> 
-
-        </main>
       </PageHeader>
+        <main className="main-teacher-list">
+          {teachers.map((teacher: Teacher) => {
+            return <TeacherItem key={teacher.id} teacher={teacher} />
+          })}
+        </main>
     </div>
   )
 }
